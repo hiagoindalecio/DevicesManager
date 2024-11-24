3db53c6807515c60dabc6117ee9547cdb963c536
hiagoindalecio <hiagoindalecio@outlook.com>
2024-11-24 18:20:39 -0300

    Creating the IOC project

---
+++ DevicesManager.CrossCutting.IOC/ConfigurationIOC.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using DevicesManager.Application.Interfaces;
+using DevicesManager.Application.Services;
+using DevicesManager.Domain.Core.Interfaces.Repositories;
+using DevicesManager.Domain.Core.Interfaces.Services;
+using DevicesManager.Domain.Services.Services;
+using DevicesManager.Infrastruture.CrossCutting.Adapter.Interfaces;
+using DevicesManager.Infrastruture.CrossCutting.Adapter.Maps;
+using DevicesManager.Infrastruture.Repository.Repositories;
+
+namespace DevicesManager.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC Application
+            builder.RegisterType<DeviceApplicationService>().As<IDeviceApplicationService>();
+            #endregion
+
+            #region IOC Services
+            builder.RegisterType<DeviceService>().As<IDeviceService>();
+            #endregion
+
+            #region IOC Repositories
+            builder.RegisterType<DeviceRepository>().As<IDeviceRepository>();
+            #endregion
+
+            #region IOC Mapper
+            builder.RegisterType<DeviceMapper>().As<IDeviceMapper>();
+            #endregion
+        }
+    }
+}
