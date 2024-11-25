e9265a0afc8dc088dc0531137c593821a48bb3f7
hiagoindalecio <hiagoindalecio@outlook.com>
2024-11-24 22:57:51 -0300

    Creating the unit tests project

---
+++ DevicesManager.Presentation.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevicesManager.Presentation.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevicesManager.Presentation.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a07e6e82-97ad-4856-83df-6abc9c1b5c31")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevicesManager.Presentation.Tests/TestDeviceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevicesManager.Presentation.Tests
+{
+    [TestClass]
+    public class TestDeviceController
+    {
+        [TestMethod]
+        public void Get_ShouldReturnAllDevices()
+        {
+            //TO DO
+        }
+    }
+}
